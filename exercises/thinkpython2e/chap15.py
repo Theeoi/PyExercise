7d723962b9594aea5358fd8e79c26790951c0008
Theodor Blom <me@theodorblom.com>
2022-08-03 22:12:03 +0200

    rect_in_circle done

--- exercises/thinkpython2e/chap15.py
+++ exercises/thinkpython2e/chap15.py
@@ -7,12 +7,24 @@ class Point:
         self.x = x
         self.y = y
 
+    def __repr__(self) -> str:
+        return f"Point({self.x}, {self.y})"
+    
+    # Want to typehint this as returning an instance, but this gives errors...
+    def init_relative(self, dx: float, dy: float):
+        x = self.x + dx
+        y = self.y + dy
+        return Point(x ,y)
+
 
 class Rectangle:
     def __init__(self, corner: Point, height: float, width: float) -> None:
         self.corner = corner
         self.height = height
         self.width = width
+
+    def __repr__(self) -> str:
+        return f"Rectangle({self.corner}, {self.height}, {self.width})"
 
     def find_center(self) -> Point:
         """
@@ -26,13 +38,22 @@ class Rectangle:
         """
         Returns the four corner Points of the Rectangle in a tuple.
         """
-        pass
+        corners = []
+        corners.append(self.corner)
+        corners.append(self.corner.init_relative(self.height, 0))
+        corners.append(self.corner.init_relative(0, self.width))
+        corners.append(self.corner.init_relative(self.height, self.width))
+
+        return tuple(corners)
 
 
 class Circle:
     def __init__(self, center: Point, radius: float) -> None:
         self.center = center
         self.radius = radius
+
+    def __repr__(self) -> str:
+        return f"Circle({self.center}, {self.radius})"
 
     def point_in_circle(self, p: Point) -> bool:
         """
@@ -51,10 +72,22 @@ def rect_in_circle(rect: Rectangle, c: Circle) -> bool:
     """
     Checks if Rectangle {rect} lies entirely within the boundary of Circle {c}.
     """
+    for corner in rect.get_corners():
+        if not c.point_in_circle(corner):
+            return False
 
-    for corner
+    return True
 
 if __name__ == "__main__":
-    c = Circle(Point(150, 100), 75)
+    mainp = Point(150, 100)
+    c = Circle(mainp, 75)
 
-    print(c.point_in_circle(Point(100, 90)))
+    p = Point(100, 90)
+    print(f"Is {p} inside {c}?")
+    print(c.point_in_circle(p))
+
+    rect = Rectangle(mainp, 60, 60)
+    print(f"Is {rect} inside {c}?")
+    print(rect_in_circle(rect, c))
+
+
