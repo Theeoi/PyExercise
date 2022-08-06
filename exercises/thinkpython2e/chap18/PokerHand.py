c5e2dd03cd8e8f304e2339c47bf84fc0e3f109c3
Theodor Blom <me@theodorblom.com>
2022-08-06 15:41:33 +0200

    Added helper code for chap18

---
+++ exercises/thinkpython2e/chap18/PokerHand.py
@@ -0,0 +1,62 @@
+"""This module contains a code example related to
+
+Think Python, 2nd Edition
+by Allen Downey
+http://thinkpython2.com
+
+Copyright 2015 Allen Downey
+
+License: http://creativecommons.org/licenses/by/4.0/
+"""
+
+from __future__ import print_function, division
+
+from Card import Hand, Deck
+
+
+class PokerHand(Hand):
+    """Represents a poker hand."""
+
+    def suit_hist(self):
+        """Builds a histogram of the suits that appear in the hand.
+
+        Stores the result in attribute suits.
+        """
+        self.suits = {}  # pylint: disable=W0201
+        for card in self.cards:
+            self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
+
+    def rank_hist(self) -> None:
+        """
+        Builds a histogram of the ranks that appear in the hand.
+        Stored in attribute ranks.
+        """
+        self.ranks = {}  # pylint: disable=W0201
+        for card in self.cards:
+            self.ranks[card.rank] = self.ranks.get(card.rank, 0) + 1
+
+    def has_flush(self):
+        """Returns True if the hand has a flush, False otherwise.
+
+        Note that this works correctly for hands with more than 5 cards.
+        """
+        self.suit_hist()
+        for val in self.suits.values():
+            if val >= 5:
+                return True
+        return False
+
+
+if __name__ == '__main__':
+    # make a deck
+    deck = Deck()
+    deck.shuffle()
+
+    # deal the cards and classify the hands
+    for i in range(7):
+        hand = PokerHand()
+        deck.move_cards(hand, 7)
+        hand.sort()
+        print(hand)
+        print(f"Flush: {hand.has_flush()}")
+        print('')
