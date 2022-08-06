a34ecc7c527da0e30d39e30c704e006d96b90f3a
Theodor Blom <me@theodorblom.com>
2022-08-06 16:26:02 +0200

    Started ex 18.3
    Implemented the different classifications for a PokerHand

--- exercises/thinkpython2e/chap18/PokerHand.py
+++ exercises/thinkpython2e/chap18/PokerHand.py
@@ -46,6 +46,116 @@ class PokerHand(Hand):
                 return True
         return False
 
+    def has_pair(self) -> bool:
+        """
+        Checks if hand contains a poker pair.
+        """
+        self.rank_hist()
+        for val in self.ranks.values():
+            if val >= 2:
+                return True
+        return False
+
+    def has_two_pair(self) -> bool:
+        """
+        Checks if hand contains a poker two pair.
+        """
+        self.rank_hist()
+        num_pairs = 0
+        for val in self.ranks.values():
+            if val >= 2:
+                num_pairs += 1
+
+        if num_pairs >= 2:
+            return True
+
+        return False
+
+    def has_three(self) -> bool:
+        """
+        Checks if hand contains a poker three of a kind.
+        """
+        self.rank_hist()
+        for val in self.ranks.values():
+            if val >= 3:
+                return True
+        return False
+
+    def has_straight(self) -> bool:
+        """
+        Checks if hand contains a poker straight.
+        """
+        ranks = [card.rank for card in self.cards]
+        ranks = tuple(set(sorted(ranks)))
+        if len(ranks) < 5:
+            return False
+        # This code does not work for 10-J-Q-K-A straights
+        # since rank[K] = 13 and rank[A] = 1
+        slen = 0
+        for i, rank in enumerate(ranks[:-1]):
+            if ranks[i+1] != rank + 1:
+                slen = 0
+            slen += 1
+            if slen >= 5:
+                return True
+        return False
+
+    def has_house(self) -> bool:
+        """
+        Checks if hand contains a poker full house.
+        """
+        self.rank_hist()
+        has_three = False
+        has_pair = False
+        for val in self.ranks.values():
+            if val >= 3:
+                has_three = True
+                continue
+            if val >= 2:
+                has_pair = True
+
+        if has_three and has_pair:
+            return True
+        return False
+
+    def has_four(self) -> bool:
+        """
+        Checks if hand contains a poker four of a kind.
+        """
+        self.rank_hist()
+        for val in self.ranks.values():
+            if val >= 4:
+                return True
+        return False
+
+    def has_straight_flush(self) -> bool:
+        """
+        Checks if hand contains poker straight flush.
+        """
+        for suit in range(4):
+            suit_ranks = []
+            for card in self.cards:
+                if card.suit == suit:
+                    suit_ranks.append(card.rank)
+            suit_ranks = tuple(set(sorted(suit_ranks)))
+            if len(suit_ranks) >= 5:
+                # This code does not work for 10-J-Q-K-A straights
+                # since rank[K] = 13 and rank[A] = 1
+                slen = 0
+                for i, rank in enumerate(suit_ranks[:-1]):
+                    if suit_ranks[i+1] != rank + 1:
+                        slen = 0
+                    slen += 1
+                    if slen >= 5:
+                        return True
+
+        return False
+
+    def classification(self) -> None:
+        """
+        Sets the label of the hand to the highest value classification.
+        """
+
 
 if __name__ == '__main__':
     # make a deck
@@ -53,10 +163,17 @@ if __name__ == '__main__':
     deck.shuffle()
 
     # deal the cards and classify the hands
-    for i in range(7):
+    for _ in range(7):
         hand = PokerHand()
         deck.move_cards(hand, 7)
         hand.sort()
         print(hand)
+        print(f"Pair: {hand.has_pair()}")
+        print(f"Two pair: {hand.has_two_pair()}")
+        print(f"Three of a kind: {hand.has_three()}")
+        print(f"Straight: {hand.has_straight()}")
         print(f"Flush: {hand.has_flush()}")
+        print(f"Full house: {hand.has_house()}")
+        print(f"Four of a kind: {hand.has_four()}")
+        print(f"Straight flush: {hand.has_straight_flush()}")
         print('')
