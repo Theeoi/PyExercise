6ddedfee562d7f2be6d1e08986bf3a64ff34ef9b
Theodor Blom <me@theodorblom.com>
2022-08-07 22:54:38 +0200

    Finished ex 18.3

--- exercises/thinkpython2e/chap18/PokerHand.py
+++ exercises/thinkpython2e/chap18/PokerHand.py
@@ -10,6 +10,7 @@ License: http://creativecommons.org/licenses/by/4.0/
 """
 
 from __future__ import print_function, division
+from texttable import Texttable
 
 from Card import Hand, Deck
 
@@ -22,7 +23,7 @@ class PokerHand(Hand):
 
         Stores the result in attribute suits.
         """
-        self.suits = {}  # pylint: disable=W0201
+        self.suits: dict[str, int] = {}
         for card in self.cards:
             self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
 
@@ -31,7 +32,7 @@ class PokerHand(Hand):
         Builds a histogram of the ranks that appear in the hand.
         Stored in attribute ranks.
         """
-        self.ranks = {}  # pylint: disable=W0201
+        self.ranks: dict[str, int] = {}
         for card in self.cards:
             self.ranks[card.rank] = self.ranks.get(card.rank, 0) + 1
 
@@ -85,8 +86,8 @@ class PokerHand(Hand):
         """
         Checks if hand contains a poker straight.
         """
-        ranks = [card.rank for card in self.cards]
-        ranks = tuple(set(sorted(ranks)))
+        rankslist = [card.rank for card in self.cards]
+        ranks: tuple[int, ...] = tuple(set(sorted(rankslist)))
         if len(ranks) < 5:
             return False
         # This code does not work for 10-J-Q-K-A straights
@@ -133,11 +134,11 @@ class PokerHand(Hand):
         Checks if hand contains poker straight flush.
         """
         for suit in range(4):
-            suit_ranks = []
+            suit_rankslist = []
             for card in self.cards:
                 if card.suit == suit:
-                    suit_ranks.append(card.rank)
-            suit_ranks = tuple(set(sorted(suit_ranks)))
+                    suit_rankslist.append(card.rank)
+            suit_ranks: tuple[int, ...] = tuple(set(sorted(suit_rankslist)))
             if len(suit_ranks) >= 5:
                 # This code does not work for 10-J-Q-K-A straights
                 # since rank[K] = 13 and rank[A] = 1
@@ -155,14 +156,14 @@ class PokerHand(Hand):
         """
         Sets the label of the hand to the highest value classification.
         """
-        labels: tuple[str] = ('none', 'pair', 'two pair',
-                                      'three of a kind', 'straight', 'flush',
-                                      'full house', 'four of a kind',
-                                      'straight flush')
-        classification: dict[str] = {'has_pair': 1, 'has_two_pair': 2,
-                                     'has_three': 3, 'has_straight': 4,
-                                     'has_flush': 5, 'has_house': 6, 'has_four':
-                                     7, 'has_straight_flush': 8}
+        labels: tuple[str, ...] = ('None', 'Pair', 'Two pairs',
+                                   'Three of a kind', 'Straight', 'Flush',
+                                   'Full house', 'Four of a kind',
+                                   'Straight flush')
+        classification: dict[str, int] = {'has_pair': 1, 'has_two_pair': 2,
+                                          'has_three': 3, 'has_straight': 4,
+                                          'has_flush': 5, 'has_house': 6,
+                                          'has_four': 7, 'has_straight_flush': 8}
         checkers: list[str] = [f for f in dir(self) if f.startswith('has')]
         label_index: list[int] = [0]
 
@@ -172,7 +173,44 @@ class PokerHand(Hand):
                 label_index.append(classification[checker])
 
         self.label = labels[max(label_index)]
-        print(self.label)
+
+
+def count_classifications(num: int, hand_size: int = 7, p_out: bool = True) -> dict[str, int]:
+    """
+    Deals {num} hands and returns a dict mapping hand labels to their
+    frequency.
+    """
+    label_hist: dict[str, int] = {}
+    for _ in range(num):
+        deck = Deck()
+        deck.shuffle()
+
+        hand = PokerHand()
+        deck.move_cards(hand, hand_size)
+
+        hand.classification()
+        label_hist[hand.label] = label_hist.get(hand.label, 0) + 1
+
+    if p_out:
+        print(label_hist)
+        print_pokerprob(label_hist)
+
+    return label_hist
+
+
+def print_pokerprob(label_hist: dict[str, int]) -> None:
+    """
+    Prints table of simulated poker hand probabilities.
+    """
+    num_sim: int = sum(label_hist.values())
+    table = Texttable()
+    table.header(["Type", "Frequency", "Probability"])
+    table.set_cols_dtype(['t', 'i', 't'])
+    for key, value in label_hist.items():
+        table.add_row([key, value, f"{(value/num_sim)*100:.2f}"])
+
+    table.set_deco(Texttable.HEADER | Texttable.BORDER)
+    print(table.draw())
 
 
 if __name__ == '__main__':
@@ -181,19 +219,15 @@ if __name__ == '__main__':
     deck.shuffle()
 
     # deal the cards and classify the hands
-    for _ in range(7):
+    for _ in range(3):
         hand = PokerHand()
         deck.move_cards(hand, 7)
         hand.sort()
         print(hand)
-        print(f"Pair: {hand.has_pair()}")
-        print(f"Two pair: {hand.has_two_pair()}")
-        print(f"Three of a kind: {hand.has_three()}")
-        print(f"Straight: {hand.has_straight()}")
-        print(f"Flush: {hand.has_flush()}")
-        print(f"Full house: {hand.has_house()}")
-        print(f"Four of a kind: {hand.has_four()}")
-        print(f"Straight flush: {hand.has_straight_flush()}")
         print('')
 
         hand.classification()
+        print(hand.label)
+
+    NUM_HANDS: int = 100000
+    label_hist: dict[str, int] = count_classifications(NUM_HANDS)
