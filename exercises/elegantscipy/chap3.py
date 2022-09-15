05920ab03881c50b63d7d55de842ae13edb71d56
Theodor Blom <me@theodorblom.com>
2022-09-15 13:29:49 +0200

    Completed Conway GoL

--- exercises/elegantscipy/chap3.py
+++ exercises/elegantscipy/chap3.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 """
-Exercises from Chapter 3 of Elegant Scipy
+Exercises and example code from Chapter 3 of Elegant Scipy
 By: Theodor Blom
 """
+import time
+
 import numpy as np
 from skimage import io
+from scipy import ndimage as ndi
 import matplotlib.pyplot as plt
 
 URL_COINS = (
@@ -65,12 +68,104 @@ def overlay_grid(image: np.ndarray, spacing: int = 128) -> None:
     image[:, spacing:-1:spacing] = [0, 0, 255]
 
 
+def sobel_convolution(image: np.ndarray) -> None:
+    """
+    Performs sobel convolution (edge-detection) on {image} and plots the
+    result.
+    """
+    hsobel = np.array(
+        [
+            [1, 2, 1],
+            [0, 0, 0],
+            [-1, -2, -1]
+        ]
+    )
+    vsobel = hsobel.T
+
+    # Prevent overflow issues
+    image = image.astype(float) / 255
+
+    image_h = ndi.convolve(image, hsobel)
+    image_v = ndi.convolve(image, vsobel)
+    image_sobel = np.sqrt(image_h**2 + image_v**2)
+
+    fig = plt.figure()
+    gs = plt.GridSpec(2, 2, figure=fig, height_ratios=[1, 3])
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[0, 1])
+    ax3 = fig.add_subplot(gs[1, :])
+    ax1.imshow(image_h, cmap=plt.cm.RdBu)
+    ax2.imshow(image_v, cmap=plt.cm.RdBu)
+    ax3.imshow(image_sobel)
+
+
+def generate_grid(x, y, seed: int | None) -> np.ndarray:
+    """
+    Return a random ndarray of size {x} and {y} with black and white squares.
+    Optionally takes kwarg "seed: int".
+    """
+    rng = np.random.default_rng(seed)
+
+    grid = rng.integers(0, 1, (x, y), endpoint=True)
+
+    return grid
+
+
+def nextgen_filter(values: np.ndarray) -> int:
+    """
+    Returns integer for live (1) or dead (0) for center pixel depending on
+    neighbouring {values}.
+    """
+    center = values[len(values) // 2]
+    neighbour_count = np.sum(values) - center
+    if neighbour_count == 3 or (center and neighbour_count == 2):
+        return 1
+
+    return 0
+
+
+def next_generation(gen: np.ndarray) -> np.ndarray:
+    """
+    Return the next generation conway_game from {gen}
+    """
+    return ndi.generic_filter(gen, nextgen_filter, size=3, mode='wrap')
+
+
+def conway_game(size: tuple[int, int],
+                generations: int,
+                seed: int = None) -> None:
+    """
+    Implementing the Conway game of life using ndi.generic_filter.
+
+    This function is part of the exercise on page 67.
+    """
+    plt.ion()
+
+    fig, ax = plt.subplots()
+    grid = generate_grid(size[0], size[1], seed)
+    img = ax.imshow(grid, cmap='gray')
+
+    for _ in range(generations):
+        grid = next_generation(grid)
+
+        img.set_data(grid)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
+        time.sleep(0.1)
+
+
 if __name__ == "__main__":
-    make_some_noise(500, 500)
+    # make_some_noise(500, 500)
 
-    image = read_imgurl(URL_ASTRONAUT)
-    add_square(image, (50, 100), (50, 100), (0, 255, 0))
-    overlay_grid(image)
+    # image_a = read_imgurl(URL_ASTRONAUT)
+    # add_square(image_a, (50, 100), (50, 100), (0, 255, 0))
+    # overlay_grid(image_a)
+    # plt.imshow(image_a)
 
-    plt.imshow(image)
+    # image_c = read_imgurl(URL_COINS)
+    # sobel_convolution(image_c)
+
+    conway_game((20, 20), 100)
+
     plt.show()
